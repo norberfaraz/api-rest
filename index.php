844b3769e9beb73c5a0606d5c686a61f9031a748
norberfaraz <norberfaraz@gmail.com>
2015-09-27 01:34:49 -0300

    Autenticacion

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 include ("process.php");
 include ("find.php");
 require "vendor/autoload.php";
@@ -11,20 +10,38 @@ $app = new \Slim\Slim();
 $app->view(new \JsonApiView());
 $app->add(new \JsonApiMiddleware());
 
-function auth (){
-$app = \Slim\Slim::getInstance();
-$key= $app->request->headers->get('HTTP_KEY');
-echo "la key es: "+$key;
-}
+$auth= function(){
+	$app = \Slim\Slim::getInstance();
 
-$app->get('/process',  function () use ($app)  { process($app);});
+	$req=$app->request();
+	$key=$req->get('key');
+	if($key==''){
+		$app->render (401,array('msg'=>'Key incorrecto','error'=>'true'));
+	}
 
-$app->get('/process:pid', function ($pid) use ($app) { find($pid,$app);});
-//$app->get('/process/:pid', function($pid) use ($app) { find($pid,$app);});
- 
-$app->delete('/process/:pid', function($pid) use ($app) {
+	$conn = mysqli_connect('127.0.0.1','root','155070847','monitorreo');
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	$sql="select * from users where pass=".$key;
+
+	$result = $conn->query($sql);
+
+	if($result->num_rows <= 0 ){
+		$app->render (401,array('msg'=>'Key incorrecto','error'=>'true'));
+	}
+
+
+};
+
+$app->get('/process',$auth,  function () use ($app)  { process($app);});
+
+$app->get('/process/:pid',$auth,function($pid) use ($app) { find($pid,$app)
+		;});
+
+$app->delete('/process/:pid',$auth, function($pid) use ($app) {
 		$str="kill -9 ".$pid." &> prueba";
-		
+
 		$salida=posix_kill($pid,9);		
 
 		if($salida==1){
@@ -35,29 +52,29 @@ $app->delete('/process/:pid', function($pid) use ($app) {
 
 		else
 		$app->render (202,array('msg'=>'error el proceso pid='.$pid.' no se pudo eliminar, verifique si existe','error'=>'true') );	
-			
-});
+
+		});
 
 $app->get('/pri/:pri/:pid',  function ($pri,$pid) use ($app) { 
 
-			
+
 		$par="sudo ./script/prueba.sh ".$pri." ".$pid;
 		//echo $par;
 		$salida=shell_exec($par);
 		$app->render (200,array('msg'=>$salida,'error'=>'false') );
 		echo $salida;	
-	
-})
+
+		})
 ;
 
 $app->get('/create/:crt', function($crt) use ($app) {
-	$comando=$crt." &";
-	$salida=shell_exec($comando);
-	
-        $app->render(200,array(
-                'msg' => $salida,
-        ));
-    });
+		$comando=$crt." &";
+		$salida=shell_exec($comando);
+
+		$app->render(200,array(
+				'msg' => $salida,
+				));
+		});
 
 
 $app->run();
